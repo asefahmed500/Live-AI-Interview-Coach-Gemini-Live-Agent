99391563325466f78050c22fa56fb48d9c1435a5
asefahmed500 <asefahmed500@users.noreply.github.com>
2026-03-05 18:55:33 +0600

    fix: mark unused get parameter with underscore

--- apps/web/src/store/use-auth-store.ts
+++ apps/web/src/store/use-auth-store.ts
@@ -25,7 +25,7 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>()(
   persist(
-    (set, get) => ({
+    (set, _get) => ({
       user: null,
       token: null,
       _hasHydrated: false,
